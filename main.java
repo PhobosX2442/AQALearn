d0a6da198ce66834f1ec876245761a5d32fa431a
Svetoslav <quicksilverx@yandex.ru>
2025-06-03 17:26:10 +0300

    тестовый коммит 2

---
+++ main.java
@@ -0,0 +1,36 @@
+public class main {
+        public static void main(String[] args) {
+        printFizzBuzz(1, 15);
+    }
+
+
+
+//    public static void printFizzBuzz(int start, int end) {
+//        for (int i = start; i <= end; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+//}
+
+    public static void printFizzBuzz(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz " + i);
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz " + i);
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz " + i);
+            } else {
+                continue;
+            }
+        }
+    }
+}
