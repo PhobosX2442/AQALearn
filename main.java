e213344f62fcf2359c8040d39fef20c969c3a003
Svetoslav <quicksilverx@yandex.ru>
2025-06-04 18:50:23 +0300

    Домашний коммит (4)

--- main.java
+++ main.java
@@ -3,34 +3,34 @@ public class main {
         printFizzBuzz(1, 15);
     }
 
-// Закомменчена базовая Вариация ДЗ
-
-//    public static void printFizzBuzz(int start, int end) {
-//        for (int i = start; i <= end; i++) {
-//            if (i % 3 == 0 && i % 5 == 0) {
-//                System.out.println("FizzBuzz");
-//            } else if (i % 3 == 0) {
-//                System.out.println("Fizz");
-//            } else if (i % 5 == 0) {
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(i);
-//            }
-//        }
-//    }
-//}
-
-// Активен вариант с ДЗ, где метод игнорирует обычные числа, подходящие прописывает сразу со значением
+// Базовая Вариация ДЗ
 
     public static void printFizzBuzz(int start, int end) {
         for (int i = start; i <= end; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz " + i);
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
-                System.out.println("Fizz " + i);
+                System.out.println("Fizz");
             } else if (i % 5 == 0) {
-                System.out.println("Buzz " + i);
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
         }
     }
 }
+
+// Вариант с ДЗ, где метод игнорирует обычные числа, подходящие прописывает сразу со значением
+
+//    public static void printFizzBuzz(int start, int end) {
+//        for (int i = start; i <= end; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz " + i);
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz " + i);
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz " + i);
+//            }
+//        }
+//    }
+//}
