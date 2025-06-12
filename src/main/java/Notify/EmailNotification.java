9cdb76ce2f539a14e4d72d2156d8bfdcaada2c3e
Svetoslav <quicksilverx@yandex.ru>
2025-06-12 05:08:49 +0300

    Теперь код рабочий

--- src/main/java/Notify/EmailNotification.java
+++ src/main/java/Notify/EmailNotification.java
@@ -3,12 +3,8 @@ package Notify;
 public class EmailNotification extends Notification{
     private String email;
 
-
     public String getEmail() {
         return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public EmailNotification(String message, String email) {
